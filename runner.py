86d86b51fd9243f221d33029188143d5324ea155
Lucas Merritt <merrittlj@protonmail.com>
2024-01-14 17:06:26 -0700

    Custom game bot
    Change runner to use custom game bot.

--- runner.py
+++ runner.py
@@ -3,20 +3,21 @@
 import os, sys, yaml
 
 # Local imports
-from brawlhalla_bot_merrittlj.game import brawlhalla_bot
-
 from brawlhalla_bot_merrittlj.util import logging_utils
 from brawlhalla_bot_merrittlj.util import hotkey_utils
+
+from brawlhalla_bot_merrittlj.game import custom_game_bot
+from brawlhalla_bot_merrittlj.game import generic_states
 
 
 def main():
     yaml_config = yaml.safe_load(open('config.yaml'))
     yaml_inputs_data = yaml_config.get('input_keys')  # Raw input list taken from YAML that lists what input keys the program should use(ex: [input_key_left, input_key_right]) to only input "Move left" and "Move right" keys.
-    bot_input_key_dict = {}  # Dictionary translating actions to keys for consistent key actions(e.g. bot looks up key for "Light attack", instead of hard-coding specific keys).
+    bot_input_keys = {}  # Dictionary translating actions to keys for consistent key actions(e.g. bot looks up key for "Light attack", instead of hard-coding specific keys).
     for input in yaml_inputs_data:
-        bot_input_key_dict[input] = yaml_config.get(input)
+        bot_input_keys[input] = yaml_config.get(input)
 
-    bot = brawlhalla_bot.FFA_Bot(state = brawlhalla_bot.Legend_Selection, input_key_dict = bot_input_key_dict)
+    bot = custom_game_bot.Custom_Game_Bot(state = generic_states.Legend_Selection, input_keys = bot_input_keys)
 
     bot_hotkey = hotkey_utils.Hotkey(key_combination = yaml_config.get('toggle_key_combination'), activated_func = bot.program_toggle)
     bot_hotkey.run()
